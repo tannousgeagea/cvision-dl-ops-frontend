4ad03decd44f8d853923700db7af0dc4913c64aa
tannousgeagea <tannous.geagea@wasteant.com>
2025-01-13 09:51:49 +0100

    adjuest dataset actions

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import UploadPage from './pages/upload/upload-page';
 import Layout from './components/ui/common/layout';
 import ImageAnnotation from './pages/annotate/annotate';
 import Dataset from './pages/dataset/dataset';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Sidebar from './components/ui/common/sidebar';
 import ProjectLayout from './components/ui/common/project-layout';
 
@@ -15,6 +15,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route index element={<Navigate to="/projects" replace />} />
           <Route path='/datalake' element={<Datalake />} />
           <Route path='/projects' element={<Projects />} />
           <Route path='/upload' element={<UploadPage />} />
